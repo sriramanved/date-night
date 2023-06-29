49fbdf53573e49b789e0cfcf5c6e1c436101beeb
Ved Sriraman <sriramanved@gmail.com>
2023-06-29 14:20:21 -0400

    properly rendering the chat responses

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -3,6 +3,7 @@
 import { MessagesContext } from '@/context/messages'
 import { cn } from '@/lib/utils'
 import { FC, HTMLAttributes, useContext } from 'react'
+import MarkdownLite from './MarkdownLite'
 // import MarkdownLite from './MarkdownLite'
 
 interface ChatMessagesProps extends HTMLAttributes<HTMLDivElement> {}
@@ -36,8 +37,7 @@ const ChatMessages: FC<ChatMessagesProps> = ({ className, ...props }) => {
                     'bg-blue-600 text-white': message.isUserMessage,
                     'bg-gray-200 text-gray-900': !message.isUserMessage,
                   })}>
-                  {/* <MarkdownLite text={message.text} /> */}
-                  {message.text}
+                  <MarkdownLite text={message.text} />
                 </p>
               </div>
             </div>

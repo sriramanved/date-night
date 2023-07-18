7726ba279a54492405044fac7c70d6537b3234d5
Ved Sriraman <sriramanved@gmail.com>
2023-07-17 22:14:17 -0400

    finished inlining autocompletion loc, bug fixes

--- src/components/places/PlacesSearchForm.tsx
+++ src/components/places/PlacesSearchForm.tsx
@@ -4,6 +4,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import {
+  Command,
+  CommandInput,
+  CommandList,
+  CommandItem,
+  CommandEmpty,
+  CommandGroup,
+} from "@/components/ui/Command";
+import {
   Form,
   FormControl,
   FormDescription,
@@ -12,11 +20,17 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/Form";
-import { Checkbox } from "@/components/ui/Checkbox";
-import AutocompleteLocation from "./AutocompleteLocation";
-import { toast } from "@/hooks/use-toast";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/Select";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
+import { Checkbox } from "@/components/ui/Checkbox";
+import { toast } from "@/hooks/use-toast";
 import { usePlacesContext } from "@/(contexts)/places";
 import {
   pricingOptions,
@@ -30,14 +44,6 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import debounce from "lodash.debounce";
 import { useOnClickOutside } from "@/hooks/use-on-click-outside";
-import {
-  Command,
-  CommandInput,
-  CommandList,
-  CommandItem,
-  CommandEmpty,
-  CommandGroup,
-} from "@/components/ui/Command";
 
 interface Location {
   id: string;
@@ -71,6 +77,8 @@ export function LocationForm() {
     usePlacesContext();
   const [radiusValue, setRadiusValue] = useState(6.21371);
   const [input, setInput] = useState("");
+  const commandRef = useRef<HTMLDivElement>(null);
+  const [showSearchResults, setShowSearchResults] = useState(false);
 
   const request = debounce(async () => {
     refetch();
@@ -81,6 +89,11 @@ export function LocationForm() {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useOnClickOutside(commandRef, () => {
+    form.setValue("locationName", input);
+    setShowSearchResults(false);
+  });
 
   const {
     isFetching,
@@ -148,12 +161,17 @@ export function LocationForm() {
               <FormItem className="flex flex-col">
                 <FormLabel>Location</FormLabel>
                 <FormControl>
-                  <Command className="relative rounded-lg border max-w-lg overflow-visible">
+                  <Command
+                    ref={commandRef}
+                    className="relative rounded-lg border max-w-lg overflow-visible"
+                  >
                     <CommandInput
                       onValueChange={(text) => {
                         setInput(text);
                         debounceRequest();
                         form.setValue("locationName", text);
+                        setInput(text);
+                        setShowSearchResults(true);
                       }}
                       value={field.value}
                       className="custom-input outline-none border-none focus:border-none focus:outline-none ring-0"
@@ -168,29 +186,33 @@ export function LocationForm() {
 
                         {locations && locations.length > 0 && (
                           <CommandGroup heading="Locations">
-                            {locations.map((location: Location) => (
-                              <CommandItem
-                                key={location.id}
-                                value={`${location.name}-${location.adminDivision1.name}`}
-                                onSelect={() => {
-                                  const locationString = `${location.name}, ${location.adminDivision1.name}, USA`;
-                                  form.setValue("locationName", locationString);
-                                  setInput(locationString);
-                                }}
-                              >
-                                <div>
-                                  <span>
-                                    {location.name},{" "}
-                                    {location.adminDivision1.name}, USA
-                                  </span>
-                                  <br />
-                                  <small>
-                                    Lat: {location.coordinates.latitude}, Lng:{" "}
-                                    {location.coordinates.longitude}
-                                  </small>
-                                </div>
-                              </CommandItem>
-                            ))}
+                            {showSearchResults &&
+                              locations.map((location: Location) => (
+                                <CommandItem
+                                  key={location.id}
+                                  value={`${location.name}-${location.adminDivision1.name}`}
+                                  onSelect={() => {
+                                    const locationString = `${location.name}, ${location.adminDivision1.name}, USA`;
+                                    form.setValue(
+                                      "locationName",
+                                      locationString
+                                    );
+                                    setInput(locationString);
+                                  }}
+                                >
+                                  <div>
+                                    <span>
+                                      {location.name},{" "}
+                                      {location.adminDivision1.name}, USA
+                                    </span>
+                                    <br />
+                                    <small>
+                                      Lat: {location.coordinates.latitude}, Lng:{" "}
+                                      {location.coordinates.longitude}
+                                    </small>
+                                  </div>
+                                </CommandItem>
+                              ))}
                           </CommandGroup>
                         )}
                       </CommandList>
@@ -262,6 +284,139 @@ export function LocationForm() {
                 </FormControl>
                 <FormDescription>
                   Include any optional terms to aid in your search.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="radius"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>
+                  Search Radius (in miles): {radiusValue.toFixed(2)} miles
+                </FormLabel>
+                <FormControl>
+                  <Slider
+                    defaultValue={[radiusValue]}
+                    max={24.854848}
+                    min={1}
+                    step={0.01}
+                    onValueChange={(values) => {
+                      field.onChange(values[0]);
+                      setRadiusValue(values[0]);
+                    }}
+                  />
+                </FormControl>
+                <FormDescription>
+                  This is the search radius used for your search. It might be
+                  adjusted by Yelp based on business density.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="open_now"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value || false}
+                    onCheckedChange={(checkedState) => {
+                      if (checkedState === "indeterminate") return;
+                      field.onChange(checkedState);
+                    }}
+                  />
+                </FormControl>
+                <div className="space-y-1 leading-none">
+                  <FormLabel>Open Now</FormLabel>
+                  <FormDescription>
+                    Select this option to only return the businesses that are
+                    open now.
+                  </FormDescription>
+                </div>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="sort_by"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Sort By</FormLabel>
+                <Select
+                  onValueChange={(value) => {
+                    field.onChange(value);
+                    if (!validSortOptions.has(value)) {
+                      throw new Error(`Invalid sort option: ${value}`);
+                    }
+                  }}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select a sorting option" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="best_match">Best Match</SelectItem>
+                    <SelectItem value="rating">Rating</SelectItem>
+                    <SelectItem value="review_count">Review Count</SelectItem>
+                    <SelectItem value="distance">Distance</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormDescription>
+                  Suggest to Yelp's search algorithm that the results be sorted
+                  by the selected option.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="attributes"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Flairs</FormLabel>
+                <FormControl>
+                  <div>
+                    {filterAttributes.map((attribute) => (
+                      <FormItem
+                        key={attribute.id}
+                        className="flex flex-row items-start space-x-3 space-y-0"
+                      >
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value?.includes(attribute.id)}
+                            onCheckedChange={(checked) => {
+                              return checked
+                                ? field.value
+                                  ? field.onChange([
+                                      ...field.value,
+                                      attribute.id,
+                                    ])
+                                  : field.onChange([attribute.id])
+                                : field.onChange(
+                                    field.value?.filter(
+                                      (value) => value !== attribute.id
+                                    )
+                                  );
+                            }}
+                          />
+                        </FormControl>
+                        <FormLabel className="text-sm font-normal">
+                          {attribute.label}
+                        </FormLabel>
+                      </FormItem>
+                    ))}
+                  </div>
+                </FormControl>
+                <FormDescription>
+                  Select the attributes you are interested in.
                 </FormDescription>
                 <FormMessage />
               </FormItem>
